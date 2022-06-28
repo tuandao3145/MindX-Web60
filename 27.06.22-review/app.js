643d0111d171003f9a37dbb2a609a7e4606c0da1
Tuan Dao <daotuan.bk@gmail.com>
2022-06-29 01:18:03 +0700

    submit ex 01 - 27.06.22

---
+++ 27.06.22-review/app.js
@@ -0,0 +1,36 @@
+Car = function (make, speed) {
+	this.make = make;
+	this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+	this.speed += 10;
+	console.log("The speed of ", this.make, " car is ", this.speed, " KM/H");
+};
+
+Car.prototype.brake = function () {
+	this.speed -= 5;
+	console.log("The speed of ", this.make, " car is ", this.speed, " KM/H");
+};
+
+let BMW = new Car("BMW", 120);
+let Mercedes = new Car("Mercedes", 95);
+
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+BMW.accelerate();
+
+BMW.brake();
+BMW.brake();
+BMW.brake();
+BMW.brake();
+
+Mercedes.accelerate();
+Mercedes.accelerate();
+Mercedes.accelerate();
+
+Mercedes.brake();
+Mercedes.brake();
+Mercedes.brake();
